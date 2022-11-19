f0ab8dcba14e849d9d4015deb4cc1a8c580dbae2
dannyrojas28 <rojas2895@gmail.com>
2022-11-19 10:18:44 -0500

    config and connection at mongodb
    Signed-off-by: dannyrojas28 <rojas2895@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DevicesModule } from './app/devices/devices.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development'],
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(
+      `${process.env.DB_PREFIX}://${process.env.DB_USERNAME}:${
+        process.env.DB_PASSWORD + '@' + process.env.DB_HOST + process.env.DB
+      }`,
+    ),
+    DevicesModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
