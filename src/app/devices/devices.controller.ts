de0c1da2fd724695ac693bf21d67f0800b39c985
dannyrojas28 <rojas2895@gmail.com>
2022-11-23 17:06:14 -0500

    add docker and redis
    Signed-off-by: dannyrojas28 <rojas2895@gmail.com>

--- src/app/devices/devices.controller.ts
+++ src/app/devices/devices.controller.ts
@@ -7,14 +7,25 @@ import {
   Patch,
   Post,
   Query,
+  CACHE_MANAGER,
+  UseInterceptors,
+  CacheInterceptor,
+  Inject,
 } from '@nestjs/common';
 import { DevicesService } from './devices.service';
 import { DevicesDto } from './dto/devices.dto';
 import { PaginationDto } from './dto/pagination.dto';
+import { Cache } from 'cache-manager';
 
+@UseInterceptors(CacheInterceptor)
 @Controller('devices')
 export class DevicesController {
-  constructor(private readonly devicesService: DevicesService) {}
+  //This would be our dummy database since we won't be connecting to a database in the article
+  randomNumDbs = Math.floor(Math.random() * 10);
+  constructor(
+    private readonly devicesService: DevicesService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
 
   @Post()
   create(@Body() devicesDto: DevicesDto): object {
@@ -27,8 +38,26 @@ export class DevicesController {
   }
 
   @Get()
-  findAll(@Query() query, @Query() paginationDto: PaginationDto): object {
-    return this.devicesService.GetAll(query, paginationDto);
+  async findAll(
+    @Query() query,
+    @Query() paginationDto: PaginationDto,
+  ): Promise<object> {
+    let data = await this.cacheManager.get('getAll');
+    if (data) {
+      return {
+        data,
+        FromRedis: 'this is loaded from redis cache',
+      };
+    }
+
+    if (!data) {
+      data = this.devicesService.GetAll(query, paginationDto);
+      await this.cacheManager.set('getAll', data);
+      return {
+        data,
+        FromRandomNumDbs: 'this is loaded from randomNumDbs',
+      };
+    }
   }
 
   @Get(':id')
--- src/app/devices/devices.module.ts
+++ src/app/devices/devices.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { CacheInterceptor, CacheModule, Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
 import { DevicesController } from './devices.controller';
 import { DevicesService } from './devices.service';
@@ -7,8 +8,15 @@ import { Devices, DevicesSchema } from './schema/device.schema';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Devices.name, schema: DevicesSchema }]),
+    CacheModule.register(),
   ],
   controllers: [DevicesController],
-  providers: [DevicesService],
+  providers: [
+    DevicesService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class DevicesModule {}
